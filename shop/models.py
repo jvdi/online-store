411865b218f33dd5b30927144d67d26b520a7b10
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-22 13:26:39 +0430

    Add - Field:price_old and set name for title:product->data

--- shop/models.py
+++ shop/models.py
@@ -6,8 +6,12 @@ class Product(models.Model):
     insert_date = models.DateTimeField(verbose_name="ژمان ورود محصول به فروشگاه")
     selling = models.IntegerField(verbose_name="تعداد فروش انجام شده محصول", null=True, blank=True)
     price = models.BigIntegerField(verbose_name="قیمت محصول", null=True, blank=True)
+    price_old = models.BigIntegerField(verbose_name="قیمت قبلی محصول", null=True, blank=True)
     image = models.ImageField(upload_to='mysite/shop/static/shop/images', verbose_name='تصویر محصول', null=True)
-    
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = "محصول"
         verbose_name_plural= "محصولات"
