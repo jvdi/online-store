fdad3e400cd2a546f9d84273fc79a82ca758be2c
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-22 11:13:10 +0430

    Create - Model for database of product

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="نام محصول")
+    special = models.BooleanField(verbose_name="محصول ویژه")
+    insert_date = models.DateTimeField(verbose_name="ژمان ورود محصول به فروشگاه")
+    selling = models.IntegerField(verbose_name="تعداد فروش انجام شده محصول", null=True, blank=True)
+    price = models.BigIntegerField(verbose_name="قیمت محصول", null=True, blank=True)
+    image = models.ImageField(upload_to='mysite/shop/static/shop/images', verbose_name='تصویر محصول', null=True)
+    
+    class Meta:
+        verbose_name = "محصول"
+        verbose_name_plural= "محصولات"
