9449852898c6f76ad41339d4c36b5e3252319005
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-22 13:27:55 +0430

    Write - View for use product in page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Product
+
 def index(request):
+    # dictionary for send data to template
     context={}
+    # Append data to dictionary
+    context['product']=Product.objects.all()
     return render(request, 'shop/index.html', context)
