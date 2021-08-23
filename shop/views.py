49cace72462f6628dc87d06beedd7a5eb46ed262
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-23 12:28:05 +0430

    Write -> View for show latest and top-selling products

--- shop/views.py
+++ shop/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 from .models import Product
 
 def index(request):
+    latest_product_list = Product.objects.order_by('-insert_date')[:8]
+    top_selling_product_list = Product.objects.order_by('-selling')[:8]
     # dictionary for send data to template
-    context={}
+    context={
+        'latest_product_list':latest_product_list,
+        'top_selling_product_list':top_selling_product_list,
+        }
     # Append data to dictionary
     context['product']=Product.objects.all()
     return render(request, 'shop/index.html', context)
