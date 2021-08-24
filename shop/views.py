6137ea9397bdbc6708a0963a423a3fd6d0cdce4a
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-24 09:39:11 +0430

    Improve - View for special product list

--- shop/views.py
+++ shop/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from .models import Product
 
 def index(request):
+    special_product_list = Product.objects.filter(special=1).order_by('-insert_date')
     latest_product_list = Product.objects.order_by('-insert_date')[:8]
-    top_selling_product_list = Product.objects.order_by('-selling')[:8]
+    top_selling_product_list = Product.objects.order_by('-selling')
     # dictionary for send data to template
     context={
+        'special_product_list':special_product_list,
         'latest_product_list':latest_product_list,
         'top_selling_product_list':top_selling_product_list,
         }
-    # Append data to dictionary
-    context['product']=Product.objects.all()
     return render(request, 'shop/index.html', context)
