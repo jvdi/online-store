840fdd6fc63c6dcdb592d3613ae406b091afc8fc
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-28 16:10:19 +0430

    Add - limit to show top selling products

--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,7 @@ from .models import Product
 def index(request):
     special_product_list = Product.objects.filter(special=1).order_by('-insert_date')
     latest_product_list = Product.objects.order_by('-insert_date')[:8]
-    top_selling_product_list = Product.objects.order_by('-selling')
+    top_selling_product_list = Product.objects.order_by('-selling')[:8]
     # dictionary for send data to template
     context={
         'special_product_list':special_product_list,
