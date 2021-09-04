c8520b7cccb45b6612d13baa57d35a0a8dd2922c
Mohammad Javidi <m.javidii@yahoo.com>
2021-09-04 12:59:20 +0430

    Refactor code - Merge and shorten by functions and ...

--- shop/static/shop/js/cart.js
+++ shop/static/shop/js/cart.js
@@ -42,3 +42,87 @@ open.onsuccess = function() {
     };
 }
 };
+
+function check_cart(id){
+    // Open (or create) the database
+    var open = indexedDB.open("site_data");
+    
+    // Create the schema
+    open.onupgradeneeded = function() {
+        var db = open.result;
+        var store = db.createObjectStore("cart", {keyPath: "id"});
+        var index = store.createIndex("NameIndex", ["id",]);
+    };
+    
+    open.onsuccess = function() {
+        // Start a new transaction
+        var db = open.result;
+        var tx = db.transaction("cart", "readwrite");
+        var store = tx.objectStore("cart");
+        var index = store.index("NameIndex");
+    
+        // Query the data
+        var getId = store.get(id);
+        getId.onsuccess = function() {
+            if (typeof getId.result !== 'undefined') {
+                // console.log(getId.result.amount);  // => "amount"
+                var a = getId.result.amount
+                document.getElementById('amount-'+id).innerHTML = a;
+            }else{
+                document.getElementById('amount-'+id).innerHTML = 0;
+            }
+        };   
+    
+        // Close the db when the transaction is done
+        tx.oncomplete = function() {
+            db.close();
+        };
+    }    
+};
+
+
+function remove_cart(id){
+    // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+    var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+    
+    // Open (or create) the database
+    var open = indexedDB.open("site_data");
+    
+    // Create the schema
+    open.onupgradeneeded = function() {
+        var db = open.result;
+        var store = db.createObjectStore("cart", {keyPath: "id"});
+        var index = store.createIndex("NameIndex", ["id",]);
+    };
+    
+    open.onsuccess = function() {
+        // Start a new transaction
+        var db = open.result;
+        var tx = db.transaction("cart", "readwrite");
+        var store = tx.objectStore("cart");
+        var index = store.index("NameIndex");
+    
+        // Query the data
+        var getId = store.get(id);
+        getId.onsuccess = function() {
+            if (typeof getId.result !== 'undefined') {
+                // console.log(getId.result.amount);  // => "amount"
+                var a = getId.result.amount
+                var b = a-=1
+                if (b <= 0) {
+                    // alert("zero");
+                    store.delete(id);
+                    document.getElementById('amount-'+id).innerHTML = 0;
+                }else{
+                store.put({id: id, amount: b});
+                document.getElementById('amount-'+id).innerHTML = b;
+                }
+            }
+        };   
+    
+        // Close the db when the transaction is done
+        tx.oncomplete = function() {
+            db.close();
+        };
+    }
+};
