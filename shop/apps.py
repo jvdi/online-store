360cb147d2c03ccfa4f40f84235e036f650604f0
Mohammad Javidi <m.javidii@yahoo.com>
2021-10-13 17:28:31 +0330

    Add Comment for verbose_name:app

--- shop/apps.py
+++ shop/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+    # translate - shop name to farsi
     verbose_name = 'فروشگاه'
